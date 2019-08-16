b8af065555959bd2350e092a9d67ee6f5d664c73
Emett Speer <speer.emett@gmail.com>
2019-08-16 08:43:44 -0700

    Updated to fix issues with my code

--- src/getURLParams.js
+++ src/getURLParams.js
@@ -1,15 +1,20 @@
 module.exports = (url) => {
-  if (typeof url !== 'string' || !(url instanceof URL)) {
+  const typeOfUrl = typeof url;
+  if (typeOfUrl === 'string' || url instanceof URL) {
+    if (typeOfUrl === 'string') {
+      url = new URL(url);
+    }
+
+    const params = new URLSearchParams(url.search);
+
+    const paramsObj = {};
+    params.forEach(function(value, key) {
+      console.log(value, key);
+      paramsObj[key] = value;
+    });
+
+    return paramsObj;
+  } else {
     throw TypeError('url must be of type String or URL');
   }
-
-  const { search } = new URL(url);
-  const params = new URLSearchParams(search);
-
-  const paramsObj = {};
-  params.forEach(function(value, key) {
-    paramsObj[key] = value;
-  });
-
-  return paramsObj;
 };
