23db8a9a1e5fb53527cca301810d844e8a0b0cdb
Eric Lakatos <ericlakatos@outlook.com>
2019-08-17 02:10:09 -0400

    Shifted error conditions and moved config a bit

--- src/quickSortArrayObjects.js
+++ src/quickSortArrayObjects.js
@@ -7,43 +7,38 @@
  * @example
  * quickSortArrayObjects([{date: '2099-01-01'}, {date: '1099-01-01'}], {type: 'date', 'property': 'releaseDate'})
  */
-module.exports = (list, config = undefined) => {
-  const options = {
-    type: undefined,
-    property: undefined,
-    asc: true,
+module.exports = (list, { type, property, asc = true } = {}) => {
+  let options = {
+    type,
+    property,
+    asc,
   };
 
-  if (Array.isArray(list)) {
-    if (list.length === 1) {
-      return list;
-    }
+  if (!Array.isArray(list)) {
+    throw TypeError('You must pass in an array');
+  }
 
-    if (config) {
-      Object.assign(options, config);
-    }
+  if (list.length === 1) {
+    return list;
+  }
 
-    const { type, property, asc } = options;
-    const filtered = list.filter(item => Object.keys(item).includes(property));
+  const filtered = list.filter(item => Object.keys(item).includes(property));
 
-    if (filtered.length > 0) {
-      switch (type) {
-        case 'date':
-          return list.sort(function(a, b) {
-            if (asc) {
-              return new Date(b[property]) - new Date(a[property]);
-            } else {
-              return new Date(a[property]) - new Date(b[property]);
-            }
-          });
+  if (filtered.length > 0) {
+    switch (options.type) {
+      case 'date':
+        return list.sort(function(a, b) {
+          if (options.asc) {
+            return new Date(b[options.property]) - new Date(a[options.property]);
+          } else {
+            return new Date(a[options.property]) - new Date(b[options.property]);
+          }
+        });
 
-        default:
-          throw TypeError('supplied config did not match available sorting');
-      }
-    } else {
-      throw TypeError('no objects were passed in to sort');
+      default:
+        throw TypeError('supplied config did not match available sorting');
     }
   } else {
-    throw TypeError('You must pass in an array');
+    throw TypeError('no objects were passed in to sort');
   }
 };
